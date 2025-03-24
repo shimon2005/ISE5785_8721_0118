0e2a596f94874bfede699e72407f6d0595813e41
shimon khakshour <shimony2017@gmail.com>
2025-03-24 16:29:19 +0200

    unit test 1

---
+++ src/unittests/primitives/PointTests.java
@@ -0,0 +1,11 @@
+package unittests.primitives;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PointTests {
+    @Test
+    void additionTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ src/unittests/primitives/VectorTests.java
@@ -0,0 +1,34 @@
+package unittests.primitives;
+
+import org.junit.jupiter.api.Test;
+
+class VectorTests {
+
+    @Test
+    void dotProduct() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void crossProduct() {
+    }
+
+    @Test
+    void lengthSquared() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void normalize() {
+    }
+}
